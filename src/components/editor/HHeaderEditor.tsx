c34049ddba8b5bb983c3e7682a1027b4551d765c
yogis <yogisyo16@gmail.com>
2025-08-12 12:20:46 +0700

    new build

--- src/components/editor/HHeaderEditor.tsx
+++ src/components/editor/HHeaderEditor.tsx
@@ -5,18 +5,18 @@ import useColors from '../../themes/colors';
 import useIsMobile from "../../utils/isMobile";
 
 interface Props {
-    anchorEl: null | HTMLElement;
-    valueSelect: string;
-    isPasteEnabled: boolean;
-    onBack: () => void;
-    onUndo: () => void;
-    onRedo: () => void;
-    onRevert: () => void;
-    onCopyEdit: () => void;
-    onPasteEdit: () => void;
-    onMenuClick: (event: React.MouseEvent<HTMLElement>) => void;
-    onMenuClose: () => void;
-    onSelectButton: () => void;
+    anchorEl?: null | HTMLElement;
+    valueSelect?: string;
+    isPasteEnabled?: boolean;
+    onBack?: () => void;
+    onUndo?: () => void;
+    onRedo?: () => void;
+    onRevert?: () => void;
+    onCopyEdit?: () => void;
+    onPasteEdit?: () => void;
+    onMenuClick?: (event: React.MouseEvent<HTMLElement>) => void;
+    onMenuClose?: () => void;
+    onSelectButton?: () => void;
 }
 
 export default function HHeaderEditor(props: Props) {
