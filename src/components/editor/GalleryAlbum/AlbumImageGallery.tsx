2f913f38c6344d3156dfb7a988cfe191681eccd3
yogis <yogisyo16@gmail.com>
2025-08-14 16:31:02 +0700

    added scrolling

--- src/components/editor/GalleryAlbum/AlbumImageGallery.tsx
+++ src/components/editor/GalleryAlbum/AlbumImageGallery.tsx
@@ -38,7 +38,7 @@ const AlbumImageGallery: React.FC<ImageGalleryProps> = (props) => {
     console.log("imageCollection: ", imageCollection);
 
 	return (
-		<section style={{ width: '100%' }}>
+		<section style={{ width: '100%', maxHeight: '100%', overflowY: 'auto' }}>
 			<ResponsiveMasonry columnsCountBreakPoints={{ 750: 2, 900: 4 }}>
 				<Masonry>
 					{imageCollection.map((photo, index) => {
